8167e145393b809c0ac5c2e38dcb46668191da5e
Sarvani Kandukuri <sarvanik60@gmail.com>
2023-01-27 22:06:10 +0530

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar(){
+    return(
+        <>
+            <div>
+                <ul className="">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Project</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </>
+    )
+}
